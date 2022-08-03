bf99582c1c436638b2963cc1e119675dd20a4126
Cassandra Jenisch <cassandrajenisch@Cassandras-MacBook-Air.local>
2022-08-03 23:18:48 +1200

    Dashboard

---
+++ dashboards/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Dashboard
+
+# Create your views here.
+def index(request):
+    obj = Dashboard.objects.get(id=1)
+    context = {
+        'obj' : obj
+    }
+    return render(request, 'dashboard/index.html', context)
--- dockinnqueue/settings.py
+++ dockinnqueue/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'qr_code',
+    'dashboards.apps.DashboardsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'dockinnqueue.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'dockinnqueue/static')
+]
+
+# Media Foder Settings 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
