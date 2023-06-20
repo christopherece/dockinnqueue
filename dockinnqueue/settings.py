0cc31ed45bcb79db4e6f2bc6092f1353ed0ad29a
meztroinhinyero <christopheranchetaece@gmail.com>
2023-06-20 20:39:55 +1200

    Added Qrcode

--- dockinnqueue/settings.py
+++ dockinnqueue/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'dockinnqueue.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tbl_dockinn',
+        'NAME': 'dockinn_tbl',
         'USER': 'postgres',
         'PASSWORD': '!pass1234',
         'HOST':'192.168.10.225'
